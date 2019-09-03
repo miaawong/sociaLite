1d28f1c6691f0cb26e8b0979528d67514876befd
Mia Wong <miaw629@gmail.com>
2019-09-03 15:33:34 -0400

    switched to express, since our api is hitting the same location but we have two very different functions, it would be cleaner to use express

---
+++ dbschema.js
@@ -0,0 +1,12 @@
+//how our data is gonna look like, just as a reference
+let db = {
+    thoughts: [
+        {
+            userHandle: "user",
+            body: "thoughtzzz",
+            createdAt: "2019-09-03T19:22:35.302Z",
+            heartCount: 5,
+            commentCount: 2
+        }
+    ]
+};
--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,43 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const serviceAccount = require("./socialite-780ad-firebase-adminsdk-7a6qz-0b8e5d3d20.json");
-
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello Mia");
-});
+const express = require("express");
+const app = express();
 
-exports.getThoughts = functions.https.onRequest((req, res) => {
+app.get("/thoughts", (req, res) => {
     admin
         .firestore()
         .collection("thoughts")
+        .orderBy("createdAt", "desc")
         .get()
         .then(data => {
             let thoughts = [];
             data.forEach(doc => {
-                thoughts.push(doc.data());
+                thoughts.push({
+                    thoughtId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
             });
             return res.json(thoughts);
         })
         .catch(err => console.error(err));
 });
 
-exports.createThought = functions.https.onRequest((req, res) => {
+app.post("/thought", (req, res) => {
+    // to prevent a get request with a status code of 400 instead of 500
+    // !!! not needed anymore since we switch to express, which handles this for us
+    // if (req.method !== "POST") {
+    //     return res.status(400).json({ error: "Method not allowed" });
+    // }
     const newThought = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
     admin
         .firestore()
@@ -45,3 +51,5 @@ exports.createThought = functions.https.onRequest((req, res) => {
             console.error(err);
         });
 });
+
+exports.api = functions.https.onRequest(app);
