4c04f3e31c6f263489a7d97a70c608b61c05b32b
Mia Wong <miaw629@gmail.com>
2019-09-03 00:12:10 -0400

    fixed credentials for localhost for firebase, changed node version to 8

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const serviceAccount = require("./socialite-780ad-firebase-adminsdk-7a6qz-0b8e5d3d20.json");
 
-admin.initializeApp();
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -22,4 +25,23 @@ exports.getThoughts = functions.https.onRequest((req, res) => {
             return res.json(thoughts);
         })
         .catch(err => console.error(err));
+});
+
+exports.createThought = functions.https.onRequest((req, res) => {
+    const newThought = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    };
+    admin
+        .firestore()
+        .collection("thoughts")
+        .add(newThought)
+        .then(doc => {
+            res.json({ message: `document ${doc.id} created successfully` });
+        })
+        .catch(err => {
+            res.status(500).json({ error: "oops, something went wrong" });
+            console.error(err);
+        });
 });
